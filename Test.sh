02b199865723633759c80df3ca7d287fb8b12b96
ajw <>
2009-01-05 12:28:22 +0000

    Square tests

--- Test.sh
+++ Test.sh
@@ -1,5 +1,7 @@
 rm -rf Test/Dst/* >/dev/null 2>&1
 mkdir Test/Dst/Landscape
 mkdir Test/Dst/Portrait
+mkdir Test/Dst/Square
 Src/FrameResize -v Test/Src Test/Dst/Landscape 800x480
 Src/FrameResize -v Test/Src Test/Dst/Portrait 480x800
+Src/FrameResize -v Test/Src Test/Dst/Square 480x480
