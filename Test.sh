923e3acb12350cc3a037484559f1165412d98fa8
ajw <>
2011-03-20 11:02:47 +0000

    bring cvs up to date

--- Test.sh
+++ Test.sh
@@ -2,6 +2,21 @@ rm -rf Test/Dst/* >/dev/null 2>&1
 mkdir Test/Dst/Landscape
 mkdir Test/Dst/Portrait
 mkdir Test/Dst/Square
+
+echo
+echo Landscape test
+echo ==============
+echo
 Src/FrameResize -v Test/Src Test/Dst/Landscape 800x480
+
+echo
+echo Portrait test
+echo =============
+echo
 Src/FrameResize -v Test/Src Test/Dst/Portrait 480x800
+
+echo
+echo Square test
+echo ===========
+echo
 Src/FrameResize -v Test/Src Test/Dst/Square 480x480
